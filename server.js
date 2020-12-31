50b5d804174f4d597f65b78c53755207e6fbdf52
dylan <dylanabozarth@gmail.com>
2020-12-31 14:26:09 -0800

    database

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
-const express = require ('express')
-
-const app = express()
-app.set("view engine", "ejs")
+const express = require("express");
+const mongoose = require("mongoose");
+const ShortUrl = require('./models/shorturl')
+mongoose.connect('mongodb://localhost/urlShort', {
+useNewUrlParser: true, useUnifiedTopology:true
+}) 
+const app = express();
+app.set("view engine", "ejs");
 app.get("/", (req, res) => {
-    res.render('index')
-})
-app.listen(process.env.PORT || 5000)
+  res.render("index");
+});
+app.post("/shortUrls", (req, res) => {
+    
+});
+app.listen(process.env.PORT || 5000);
