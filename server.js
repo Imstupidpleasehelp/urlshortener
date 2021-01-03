f80c80a847acdddb684350b34a079730781549fc
dylan <dylanabozarth@gmail.com>
2021-01-03 08:58:15 -0800

    whtml

--- server.js
+++ server.js
@@ -3,13 +3,12 @@ const mongoose = require('mongoose')
 const app = express()
 
 
-
-app.set('view engine', 'ejs')
+app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: false }))
 // this is how we render the views 
 app.get('/',  (req, res) => {
  
-  res.render('index')
+  res.render('app')
 })
 // the shortURL file is imported here 
 app.post('/makeurl', (req, res) => {
